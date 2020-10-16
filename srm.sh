6c8524eee82afa2cdbdc1a488cc3cf6f9b2f0e45
Aneliya-coder <aneliya.abolnik@mail.ru>
2020-10-16 20:04:50 +0000

    gzipping and moving

--- srm.sh
+++ srm.sh
@@ -4,3 +4,8 @@ if [ -e $1 ]
 then
         echo "Файл $1 будет перемещен в корзину по адресу ~/RECYCLE"
         mkdir -p ~/RECYCLE
+        gzip $1
+        mv $1.gz ~/RECYCLE
+        echo "Файл $1 сжат в архив и перемещен в корзину по адресу ~/RECYCLE"
+fi
+
